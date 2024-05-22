9cc5fdf610e7cdb7f6fee5c184ffc79e074cc98b
Said Benmoumen <said.benmumen@gmail.com>
2024-05-22 23:08:19 +0100

    bing bang updates

--- source/Popup/Popup.tsx
+++ source/Popup/Popup.tsx
@@ -1,55 +1,87 @@
-import * as React from 'react';
-import {browser, Tabs} from 'webextension-polyfill-ts';
+import * as React from "react";
 
-import './styles.scss';
+import "./styles.scss";
 
-function openWebPage(url: string): Promise<Tabs.Tab> {
-  return browser.tabs.create({url});
-}
+type Group = {
+  name: string;
+  urls: string[];
+};
 
 const Popup: React.FC = () => {
+  const [groups, setGroups] = React.useState<Group[]>([]);
+  const [creatingGroup, setCreatingGroup] = React.useState(false);
   return (
     <section id="popup">
-      <h2>WEB-EXTENSION-STARTER</h2>
-      <button
-        id="options__button"
-        type="button"
-        onClick={(): Promise<Tabs.Tab> => {
-          return openWebPage('options.html');
-        }}
-      >
-        Options Page
-      </button>
-      <div className="links__holder">
-        <ul>
-          <li>
-            <button
-              type="button"
-              onClick={(): Promise<Tabs.Tab> => {
-                return openWebPage(
-                  'https://github.com/abhijithvijayan/web-extension-starter'
-                );
-              }}
-            >
-              GitHub
-            </button>
-          </li>
-          <li>
-            <button
-              type="button"
-              onClick={(): Promise<Tabs.Tab> => {
-                return openWebPage(
-                  'https://www.buymeacoffee.com/abhijithvijayan'
-                );
-              }}
-            >
-              Buy Me A Coffee
-            </button>
-          </li>
-        </ul>
-      </div>
+      {creatingGroup ? (
+        <CreateGroup
+          onCreate={(groupName) => {
+            const exists = groups.find((g) => g.name === groupName);
+            if (exists) {
+              alert("Group already exists");
+              return;
+            }
+            setGroups((g) => [...g, { name: groupName, urls: [] }]);
+          }}
+          onCancel={() => setCreatingGroup(false)}
+        />
+      ) : (
+        <div>
+          {groups.length > 0 ? (
+            <ul>
+              {groups.map((group) => (
+                <li key={group.name}>
+                  <span>{group.name}</span>
+                  <button
+                    onClick={() => {
+                      setGroups((g) => g.filter((g) => g.name !== group.name));
+                    }}
+                  >
+                    x
+                  </button>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <h1>no groups exist</h1>
+          )}
+          <button
+            onClick={() => {
+              setCreatingGroup(true);
+            }}
+          >
+            create new group
+          </button>
+        </div>
+      )}
     </section>
   );
 };
 
 export default Popup;
+
+export const CreateGroup = ({
+  onCreate,
+  onCancel,
+}: {
+  onCreate: (groupName: string) => void;
+  onCancel: () => void;
+}) => {
+  const [groupName, setGroupName] = React.useState<string>("");
+  return (
+    <form
+      onSubmit={() => {
+        onCreate(groupName);
+      }}
+    >
+      <input
+        type="text"
+        value={groupName}
+        onChange={(e) => setGroupName(e.target.value)}
+      />
+      <button type="submit" disabled={groupName.length <= 0}>
+        Create
+      </button>
+      <button onClick={() => onCancel()}>Cancel</button>
+    </form>
+  );
+};
